b2eba25571dcea108c63490194d59356ebbd3140
Steven Anderson <happygoatdesigns@gmail.com>
2017-03-29 11:56:38 -0600

    adds content to about section, and a couple of other visual improvements

--- js/directives/stickyNavbar.js
+++ js/directives/stickyNavbar.js
@@ -78,6 +78,20 @@ angular.module('portfolioApp')
               return false;
             });
           });
+          $(function() {
+            // Select link by id and add click event
+            $('#sticky-logo-link').click(function() {
+
+              // Animate Scroll to #bottom
+              $('html,body').animate({
+                scrollTop: $("#header-section").offset().top }, // Tell it to scroll to the top #bottom
+                2000 // How long scroll will take in milliseconds
+              );
+
+              // Prevent default behavior of link
+              return false;
+            });
+          });
 
         });
       });
