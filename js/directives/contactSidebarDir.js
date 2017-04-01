8b060fc73a1d92fd3f1c6f58108f32ea1d2fa23e
Steven Anderson <happygoatdesigns@gmail.com>
2017-04-01 14:31:55 -0600

    adds content to contact section

--- js/directives/contactSidebarDir.js
+++ js/directives/contactSidebarDir.js
@@ -9,9 +9,12 @@ angular.module('portfolioApp')
       // STICKY NAVBAR WITH JQUERY //
       // var headerContainerHeight = $('.header-container').height();
       var num = $('.header-container').height(); //number of pixels before modifying styles
+      var aboutHeight = $('.home-about-me').height();
+      var developerHeight = $('.home-developer-portfolio').height();
+      var designerHeight = $('.home-design-portfolio').height();
 
       $(window).bind('scroll', function () {
-        if ($(window).scrollTop() >= num) {
+        if ($(window).scrollTop() >= num && $(window).scrollTop() <= num + aboutHeight + developerHeight + designerHeight - 400) {
           $('.contact-icons-sidebar').addClass('fixed-sidebar');
         } else {
           $('.contact-icons-sidebar').removeClass('fixed-sidebar');
